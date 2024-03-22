2f0b876583ce5d8926293356929683dae523e994
jessie <126810934+JessieNeire@users.noreply.github.com>
2024-03-22 16:44:40 -0300

    Update soma.java

---
+++ soma.java
@@ -0,0 +1,20 @@
+
+import javax.swing.JOptionPane;
+
+ 
+public class soma {
+
+    public static void main(String[] args) {
+
+     int soma=0;
+     
+     for(int k=0; k<13; k++){
+     soma=soma+k;
+     }
+     JOptionPane.showMessageDialog(null, soma);
+         
+
+    }
+}
+    
+
